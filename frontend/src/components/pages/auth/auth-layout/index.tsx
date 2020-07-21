59510753925540bccd05bdd6e885e85862ece205
Jeremy <tan_kai_qun97@yahoo.com.sg>
2020-07-21 10:47:19 +0800

    Add transition for auth layout

--- frontend/src/components/pages/auth/auth-layout/index.tsx
+++ frontend/src/components/pages/auth/auth-layout/index.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import logo from "../../../../images/treeckle-title-bottom-transparent.png";
-import { Grid, Image, Segment } from "semantic-ui-react";
+import { Grid, Image, Segment, Transition } from "semantic-ui-react";
 import "./index.scss";
 
 type Props = {
@@ -8,6 +8,12 @@ type Props = {
 };
 
 function AuthLayout({ children }: Props) {
+  const [isVisible, setVisible] = useState(false);
+
+  useEffect(() => {
+    setVisible(true);
+  }, []);
+
   return (
     <Grid
       className="auth-layout-container"
@@ -16,14 +22,24 @@ function AuthLayout({ children }: Props) {
       verticalAlign="middle"
     >
       <Grid.Column className="auth-layout-segment-container">
-        <Segment className="auth-layout-segment" placeholder raised>
-          <Grid columns="2" relaxed stackable verticalAlign="middle">
-            <Grid.Column>{children}</Grid.Column>
-            <Grid.Column className="auth-layout-image-container">
-              <Image className="auth-layout-image" src={logo} alt="Treeckle" />
-            </Grid.Column>
-          </Grid>
-        </Segment>
+        <Transition
+          onHide={() => console.log("hello")}
+          visible={isVisible}
+          animation="scale"
+        >
+          <Segment className="auth-layout-segment" placeholder raised>
+            <Grid columns="2" relaxed stackable verticalAlign="middle">
+              <Grid.Column>{children}</Grid.Column>
+              <Grid.Column className="auth-layout-image-container">
+                <Image
+                  className="auth-layout-image"
+                  src={logo}
+                  alt="Treeckle"
+                />
+              </Grid.Column>
+            </Grid>
+          </Segment>
+        </Transition>
       </Grid.Column>
     </Grid>
   );
